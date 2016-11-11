5ad2be2316775b94d2b9e668bceaa05c36478825
Max Wenger <carefreefour@gmail.com>
2016-11-11 13:02:37 -0800

    Fixed sql dependency issue.

--- InterManage.Business/ApplicationDbContext.cs
+++ InterManage.Business/ApplicationDbContext.cs
@@ -1,9 +1,13 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
 
 namespace InterManage.Business
 {
     public class ApplicationDbContext : DbContext
     {
+        // I don't know why this fixes the dependency issues... But it does.
+        Type providerService = typeof(System.Data.Entity.SqlServer.SqlProviderServices);
+
         public DbSet<Employee> Employees { get; set; }
         public DbSet<Shift> Shifts { get; set; }
     }
